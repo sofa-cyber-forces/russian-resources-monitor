5706f1465b9bc99291baf5e6b0be4a809f3e4212
sofa-cyber-forces <100516938+sofa-cyber-forces@users.noreply.github.com>
2022-02-27 17:35:04 +0200

    Add headers to the table

--- server.js
+++ server.js
@@ -58,7 +58,23 @@ function updateRegularly(url) {
 }
 
 function writeSitesInfoToHtml() {
-    let str = '<h1>Russian sites availability dashboard</h1><table style="border: 1px solid black; border-collapse: collapse"><tbody>'
+    let str = '<h1>Russian sites availability dashboard</h1>'
+    
+    str += '<table style="border: 1px solid black; border-collapse: collapse"><tbody>'
+
+    str += '<tr>'
+    str += '<th style="border: 1px solid black; border-collapse: collapse">'
+    str += '</th>'
+    str += '<th style="border: 1px solid black; border-collapse: collapse">'
+    str += 'URL'
+    str += '</th>'
+    str += '<th style="border: 1px solid black; border-collapse: collapse">'
+    str += 'Status code/Error'
+    str += '</th>'
+    str += '<th style="border: 1px solid black; border-collapse: collapse">'
+    str += 'Last update'
+    str += '</th>'
+    str += '</tr>'
 
     sitesInfo.forEach((value, key, map) => {
         let info = value
@@ -71,10 +87,6 @@ function writeSitesInfoToHtml() {
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
             str += info.url
             str += '</td">'
-            
-            str += '<td style="border: 1px solid black; border-collapse: collapse">'
-            str += 'No info'
-            str += '</td>'
         } else if (info.success) {
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
             if (info.statusCode >= 200 && info.statusCode <= 299) {
@@ -89,12 +101,10 @@ function writeSitesInfoToHtml() {
             str += '</td>'
 
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
-            str += 'Status code: '
             str += info.statusCode
             str += '</td>'
 
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
-            str += 'Last update: '
             str += info.updateTime.toISOString()
             str += '</td>'
         } else {
@@ -107,12 +117,10 @@ function writeSitesInfoToHtml() {
             str += '</td>'
 
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
-            str += 'Error: '
             str += info.error
             str += '</td>'
 
             str += '<td style="border: 1px solid black; border-collapse: collapse">'
-            str += 'Last update: '
             str += info.updateTime.toISOString()
             str += '</td>'
         }
