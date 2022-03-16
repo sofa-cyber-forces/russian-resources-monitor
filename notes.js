3aae32e49d1eda9cb9b3895dd2d0f698b27c57e7
sofa-cyber-forces <100516938+sofa-cyber-forces@users.noreply.github.com>
2022-03-16 13:15:20 +0200

    Remove raw IP-addresses from URL list

--- notes.js
+++ notes.js
@@ -1,25 +1,7 @@
 module.exports = new Map([
-    ['https://109.207.1.118/', 'IP of government services'],
-    ['https://109.207.1.97/', 'IP of government services'],
-
-    ['https://178.248.236.218:77', 'IP of Tinkoff'],
-    ['https://194.54.14.186:53', 'IP of Sberbank'],
-    ['https://194.54.14.187:53', 'IP of Sberbank'],
-    ['https://194.67.2.109:53', 'IP of Sberbank'],
-    
-    ['https://172.67.74.48:53/udp', 'IP of bitzlato.com'],
-
-    ['https://185.157.96.130', 'IP of Sberbank'],
-    ['https://84.252.144.102', 'IP of Sberbank'],
-
     ['https://www.saveukraine.org.ua/', 'Russian fake'],
-    ['https://185.104.45.162:443', 'IP of saveukraine.org.ua'],
 
     ['https://savelife.pw/', 'Russian fake'],
-
-    ['https://185.179.224.11:80', 'IP of ФСИН'],
-
-    ['https://217.012.103.135:6500', 'IP of Alfa-Bank POS-terminals'],
 
     ['https://ukraine-war.pw', 'Russian fake'],
     ['https://saveli.fe.pw', 'Russian fake'],
